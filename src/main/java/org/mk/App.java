3ba39df932ab0a4e991c349008fdfd6b2efab9a3
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2022-10-19 23:30:05 +0200

    make project runnable

--- src/main/java/org/mk/App.java
+++
@@ -1,7 +0,0 @@
-package org.mk;
-
-public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-    }
-}
---
+++ src/test/java/org/mk/experiments/AppTest.java
@@ -0,0 +1,15 @@
+package org.mk.experiments;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertThat(true).isTrue();
+    }
+}
